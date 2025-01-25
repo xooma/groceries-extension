7fc11e1caa7706f869802de1cd1f7539ef3397ca
xooma <xoomabis@gmail.com>
2025-01-25 19:07:20 +0100

    Urls matches

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,9 +1,9 @@
 export default defineContentScript({
   matches: [
-    "*://*..marmiton.org/recettes/*",
-    "*://*..jow.fr/recipes/*",
-    "*://*..latavoladigael.com/recette/*",
-    "*://*..cookomix.com/recettes/*",
+    "*://*.marmiton.org/recettes/*",
+    "*://jow.fr/recipes/*",
+    "*://*.latavoladigael.com/recette/*",
+    "*://*.cookomix.com/recettes/*",
   ],
   main() {
     console.log("Hello content.");
